b4f2ac6b1c2b6e8d73839961ce91c31955d885e8
AbhinavKamboj1 <kambojabhina04@gmail.com>
2022-07-15 16:13:43 +0530

    finalllly

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -12,12 +12,13 @@ namespace JenkinsApp
 
             Class1 math = new Class1();
             double Add = math.Add(34.5, 54.6);
-            double Sub = math.Sub(56.3, 65.7);
+            double Sub = math.Sub(67.7, 65.7);
 
             Console.WriteLine("Maths Function");
 
             Console.WriteLine(Add);
             Console.WriteLine(Sub);
+
 
             Console.ReadLine();
             Console.ReadLine();
